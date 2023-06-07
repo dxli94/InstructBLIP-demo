a6416ed18a00ad16753cbb4d44480c618a4cfe42
Dongxu Li <dongxu_li@outlook.com>
2023-06-07 15:50:36 +0800

    use vicuna 13b by default.

--- projects/instructblip/run_demo.sh
+++ projects/instructblip/run_demo.sh
@@ -1,2 +1,2 @@
-nohup python run_demo.py  --model-type vicuna7b > app.log 2>&1
-# nohup python run_demo.py  --model-type vicuna13b > app.log 2>&1
+# nohup python run_demo.py  --model-type vicuna7b > app.log 2>&1
+nohup python run_demo.py  --model-type vicuna13b > app.log 2>&1
